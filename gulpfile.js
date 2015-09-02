50746f3a7a4b2916b32a34d8cb85099cbe539ca1
long-long-float <niinikazuki@yahoo.co.jp>
2015-09-02 21:25:23 +0900

    全体的に修正
    * npm対応
    * gulpによるLivereload
    * インデントを4から2にした

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    coffee = require('gulp-coffee'),
+    webserver = require('gulp-webserver'),
+    gutil = require('gulp-util');
+
+log = function log(err) {
+  if(err.name && err.message) {
+    console.log('[ERROR]', err.name);
+    console.log(err.message);
+    console.log(err.stack);
+  }
+  else if(typeof err === 'object'){
+    for(var key in err) {
+      console.log(key, err[key]);
+    }
+  }
+  else {
+    console.log(err.toString());
+  }
+};
+
+gulp.task('coffee', function() {
+  gulp.src('src/*.coffee')
+      .pipe(coffee().on('error', log))
+      .pipe(gulp.dest('js'));
+});
+
+gulp.task('server', function() {
+  gulp.src('./')
+      .pipe(webserver({
+        livereload: {
+          enable: true,
+          filter: function(name) { return !name.match(/\.(coffee)$/); }
+        }
+      }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.coffee', ['coffee']);
+});
+
+gulp.task('default', ['server', 'watch']);
